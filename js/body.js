0f9ca70ab404acc809b78bc0dd0f5ade3e1d7286
Kenta Okubo <k017c1311@it-neec.jp>
2020-05-16 13:50:47 +0900

    ポートフォリオサイト作成完了

---
+++ js/body.js
@@ -0,0 +1,79 @@
+//do as soon as after ended loading dom elements.
+$(function(){
+  var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    effect: "cube",
+    speed: 2000,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+  });
+});
+
+$(function(){
+  new Circlebar({
+    element: ".circle-html",
+    maxValue: 40,
+    type: "progress",
+  }),
+  new Circlebar({
+    element: ".circle-css",
+    maxValue: 30,
+    type: "progress"
+  }),
+  new Circlebar({
+    element: ".circle-js",
+    maxValue: 50,
+    type: "progress"
+  }),
+  new Circlebar({
+    element: ".circle-ruby",
+    maxValue: 100,
+    type: "progress"
+  }),
+  new Circlebar({
+    element: ".circle-java",
+    maxValue: 80,
+    type: "progress"
+  });
+});
+
+// Scrollify
+$(function() {
+    $.scrollify({
+      section: '.section',
+      standardScrollElements: ".scroll",
+      easing: 'swing',
+      scrollSpeed: 1000,
+      scrollbars: true, 
+      setWrapperSize: true
+    });
+});
+
+$(function(){
+  $('.comment').on('click',function(){
+      $('.modal').fadeIn();
+      return false;
+  });
+  $('.modal-bg').on('click',function(){
+      $('.modal').fadeOut();
+      return false;
+  });
+});
+
+//モーダルウィンドウの実装
+$('.modal-muscle').modaal({});
+$('.modal-reading-book').modaal({});
+$('.modal-watching-movie').modaal({});
+$('.modal-book-minimalist').modaal({});
+$('.modal-book-philosophy').modaal({});
+$('.modal-book-business').modaal({});
+$('.modal-movie-super').modaal({});
+$('.modal-movie-strongest').modaal({});
+$('.modal-movie-sheep').modaal({});
